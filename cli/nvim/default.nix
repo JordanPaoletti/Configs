6c81bed4a7398892a8c37f5dc83105cad47553bf
Jordan Paoletti <jordanspaoletti@gmail.com>
2024-05-27 13:39:47 -0700

    run nixfmt.

--- cli/nvim/default.nix
+++ cli/nvim/default.nix
@@ -1,11 +1,10 @@
-{ config, pkgs, ... }:
-{
-    programs.neovim = {
-        enable = true;
-        defaultEditor = true;
-        extraConfig = builtins.readFile ./init.vim;
-        plugins = with pkgs.vimPlugins; [ lazy-nvim ];
-    };
+{ config, pkgs, ... }: {
+  programs.neovim = {
+    enable = true;
+    defaultEditor = true;
+    extraConfig = builtins.readFile ./init.vim;
+    plugins = with pkgs.vimPlugins; [ lazy-nvim ];
+  };
 
-    programs.zsh.shellAliases.vimdiff = "nvim -d";
+  programs.zsh.shellAliases.vimdiff = "nvim -d";
 }
