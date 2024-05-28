c50a711fefb29faf0c67e97e4509c943df5049cc
Jordan Paoletti <jordanspaoletti@gmail.com>
2024-05-28 13:39:05 -0700

    Added lazy-nvim and nvim-treesitter

--- cli/nvim/default.nix
+++ cli/nvim/default.nix
@@ -3,6 +3,7 @@
     enable = true;
     defaultEditor = true;
     extraConfig = builtins.readFile ./init.vim;
+    extraLuaConfig = builtins.readFile ./init.lua;
     plugins = with pkgs.vimPlugins; [ lazy-nvim ];
   };
 
