866d7f2be0c43763b48e0af7220c992f0b3f9ac4
Jordan Paoletti <jordanspaoletti@gmail.com>
2024-08-29 12:02:08 -0700

    add fzf

--- cli/zsh.nix
+++ cli/zsh.nix
@@ -32,7 +32,6 @@
         "git"
         "vi-mode"
         "kubectl"
-        "z"
       ];
     };
 
@@ -41,6 +40,23 @@
       "nixfmt." = "nixfmt $(find . -name '*.nix')";
       sudoe = "sudo --preserve-env=PATH env";
     };
+  };
+
+  programs.fzf = {
+    enable = true;
+    enableZshIntegration = true;
+    defaultOptions = [
+      "--ansi"
+      # https://github.com/catppuccin/fzf
+      "--color=bg+:#313244,bg:#1e1e2e,spinner:#f5e0dc,hl:#f38ba8"
+      "--color=fg:#cdd6f4,header:#f38ba8,info:#cba6f7,pointer:#f5e0dc"
+      "--color=marker:#f5e0dc,fg+:#cdd6f4,prompt:#cba6f7,hl+:#f38ba8"
+    ];
+  };
+
+  programs.zoxide = {
+  enable = true;
+  enableZshIntegration = true;
   };
 
   programs.starship = {
