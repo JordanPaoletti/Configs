e27c0e147cc950a679906064d2df4cc5ec532db5
Jordan Paoletti <jordanspaoletti@gmail.com>
2025-05-07 18:05:46 -0700

    Added nvim plugin Trouble

--- modules/desktop/cli/nvim/lua/user/init.lua
+++ modules/desktop/cli/nvim/lua/user/init.lua
@@ -2,8 +2,8 @@
 require("lazy").setup({
     -- movement
     {
-      "easymotion/vim-easymotion",
-      lazy = false
+        "easymotion/vim-easymotion",
+        lazy = false
     },
 
     -- treesitter
@@ -11,91 +11,91 @@ require("lazy").setup({
         -- Highlight, edit, and navigate code
         "nvim-treesitter/nvim-treesitter",
         dependencies = {
-          "nvim-treesitter/nvim-treesitter-context",
-          "nvim-treesitter/nvim-treesitter-textobjects",
-          "nushell/tree-sitter-nu",
+            "nvim-treesitter/nvim-treesitter-context",
+            "nvim-treesitter/nvim-treesitter-textobjects",
+            "nushell/tree-sitter-nu",
         },
         build = ":TSUpdate",
         keys = {
-          {
-            "[C",
-            function()
-              require("treesitter-context").go_to_context(vim.v.count1)
-            end,
-            silent = true,
-          },
+            {
+                "[C",
+                function()
+                    require("treesitter-context").go_to_context(vim.v.count1)
+                end,
+                silent = true,
+            },
         },
         lazy = false,
         opts = {
-          auto_install = true,
-          highlight = { enable = true },
-          indent = { enable = true },
-          incremental_selection = {
-            enable = true,
-            keymaps = {
-              node_incremental = "v",
-              node_decremental = "V",
+            auto_install = true,
+            highlight = { enable = true },
+            indent = { enable = true },
+            incremental_selection = {
+                enable = true,
+                keymaps = {
+                    node_incremental = "v",
+                    node_decremental = "V",
+                },
             },
-          },
-          textobjects = {
-            select = {
-              enable = true,
-              lookahead = true, -- Automatically jump forward to textobj, similar to targets.vim
-              keymaps = {
-                -- You can use the capture groups defined in textobjects.scm
-                ["aa"] = "@parameter.outer",
-                ["ia"] = "@parameter.inner",
-                ["af"] = "@function.outer",
-                ["if"] = "@function.inner",
-                ["ac"] = "@class.outer",
-                ["ic"] = "@class.inner",
-              },
+            textobjects = {
+                select = {
+                    enable = true,
+                    lookahead = true, -- Automatically jump forward to textobj, similar to targets.vim
+                    keymaps = {
+                        -- You can use the capture groups defined in textobjects.scm
+                        ["aa"] = "@parameter.outer",
+                        ["ia"] = "@parameter.inner",
+                        ["af"] = "@function.outer",
+                        ["if"] = "@function.inner",
+                        ["ac"] = "@class.outer",
+                        ["ic"] = "@class.inner",
+                    },
+                },
+                move = {
+                    enable = true,
+                    set_jumps = true, -- whether to set jumps in the jumplist
+                    goto_next_start = {
+                        ["]m"] = "@function.outer",
+                        ["]]"] = "@class.outer",
+                    },
+                    goto_next_end = {
+                        ["]M"] = "@function.outer",
+                        ["]["] = "@class.outer",
+                    },
+                    goto_previous_start = {
+                        ["[m"] = "@function.outer",
+                        ["[["] = "@class.outer",
+                    },
+                    goto_previous_end = {
+                        ["[M"] = "@function.outer",
+                        ["[]"] = "@class.outer",
+                    },
+                },
+                swap = {
+                    enable = true,
+                    swap_next = {
+                        ["<leader>a"] = "@parameter.inner",
+                    },
+                    swap_previous = {
+                        ["<leader>A"] = "@parameter.inner",
+                    },
+                },
             },
-            move = {
-              enable = true,
-              set_jumps = true, -- whether to set jumps in the jumplist
-              goto_next_start = {
-                ["]m"] = "@function.outer",
-                ["]]"] = "@class.outer",
-              },
-              goto_next_end = {
-                ["]M"] = "@function.outer",
-                ["]["] = "@class.outer",
-              },
-              goto_previous_start = {
-                ["[m"] = "@function.outer",
-                ["[["] = "@class.outer",
-              },
-              goto_previous_end = {
-                ["[M"] = "@function.outer",
-                ["[]"] = "@class.outer",
-              },
-            },
-            swap = {
-              enable = true,
-              swap_next = {
-                ["<leader>a"] = "@parameter.inner",
-              },
-              swap_previous = {
-                ["<leader>A"] = "@parameter.inner",
-              },
-            },
-          },
         },
         config = function(_, opts)
-          local configs = require("nvim-treesitter.configs").setup(opts)
-          require("treesitter-context").setup({
-            max_lines = 10,
-            multiline_threshold = 1,
-            -- mode = "topline",
-          })
+            local configs = require("nvim-treesitter.configs").setup(opts)
+            require("treesitter-context").setup({
+                max_lines = 10,
+                multiline_threshold = 1,
+                -- mode = "topline",
+            })
 
-          -- There are additional nvim-treesitter modules that you can use to interact
-          -- with nvim-treesitter. You should go explore a few and see what interests you:
-          --
-          --    - Incremental selection: Included, see `:help nvim-treesitter-incremental-selection-mod`
-          --    - Show your current context: https://github.com/nvim-treesitter/nvim-treesitter-context
-          --    - Treesitter + textobjects: https://github.com/nvim-treesitter/nvim-treesitter-textobjects
+            -- There are additional nvim-treesitter modules that you can use to interact
+            -- with nvim-treesitter. You should go explore a few and see what interests you:
+            --
+            --    - Incremental selection: Included, see `:help nvim-treesitter-incremental-selection-mod`
+            --    - Show your current context: https://github.com/nvim-treesitter/nvim-treesitter-context
+            --    - Treesitter + textobjects: https://github.com/nvim-treesitter/nvim-treesitter-textobjects
         end,
     },
 
@@ -119,30 +119,69 @@ require("lazy").setup({
         end,
     },
 
-    -- Formatting
+    -- Diagnostics
     {
-        "stevearc/conform.nvim",
+        "folke/trouble.nvim",
+        opts = {},
+        cmd = "Trouble",
         keys = {
             {
-                "<leader>f",
-                function()
-                    require("conform").format({ async = true })
-                end,
-                mode = "",
-                desc = "Format Buffer",
+                "<leader>xx",
+                "<cmd>Trouble diagnostics toggle<cr>",
+                desc = "Diagnostics (Trouble)",
+            },
+            {
+                "<leader>xX",
+                "<cmd>Trouble diagnostics toggle filter.buf=0<cr>",
+                desc = "Buffer Diagnostics (Trouble)",
+            },
+            {
+                "<leader>cs",
+                "<cmd>Trouble symbols toggle focus=false<cr>",
+                desc = "Symbols (Trouble)",
+            },
+            {
+                "<leader>cl",
+                "<cmd>Trouble lsp toggle focus=false win.position=right<cr>",
+                desc = "LSP Definitions / references / ... (Trouble)",
+            },
+            {
+                "<leader>xL",
+                "<cmd>Trouble loclist toggle<cr>",
+                desc = "Location List (Trouble)",
+            },
+            {
+                "<leader>xQ",
+                "<cmd>Trouble qflist toggle<cr>",
+                desc = "Quickfix List (Trouble)",
             },
         },
-        opts = {
-            formatters_by_ft = {
-                nix = { "nixfmt" },
-            },
-            format_on_save = {
-                timeout_ms = 500,
-                lsp_format = "fallback",
-            },
-        }
     },
-})
+
+        -- Formatting
+        {
+            "stevearc/conform.nvim",
+            keys = {
+                {
+                    "<leader>f",
+                    function()
+                        require("conform").format({ async = true })
+                    end,
+                    mode = "",
+                    desc = "Format Buffer",
+                },
+            },
+            opts = {
+                formatters_by_ft = {
+                    nix = { "nixfmt" },
+                },
+                format_on_save = {
+                    timeout_ms = 500,
+                    lsp_format = "fallback",
+                },
+            }
+        },
+    })
 
 
 -- Random Additions
