232330fa907ca4851a4c3505266d6ab059027327
Jordan Paoletti <jordanspaoletti@gmail.com>
2025-05-13 13:58:26 -0700

    (neovim): get conform format on save working again

--- modules/desktop/cli/nvim/lua/user/lazy/conform.lua
+++ modules/desktop/cli/nvim/lua/user/lazy/conform.lua
@@ -1,8 +1,12 @@
 require("conform").setup({
-    formatters_by_ft = {
-        bash = { "shfmt" },
-        zsh = { "shfmt" },
-        lua = { "stylua" },
-        nix = { "nixfmt" },
-    },
-});
+	formatters_by_ft = {
+		bash = { "shfmt" },
+		zsh = { "shfmt" },
+		lua = { "stylua" },
+		nix = { "nixfmt" },
+	},
+	format_on_save = {
+		timeout_ms = 500,
+		lsp_format = "fallback",
+	},
+})
