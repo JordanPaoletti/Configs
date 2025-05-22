f3dfce2f3873cae1af259a4ade37f5df86dcb7d1
Jordan Paoletti <jordanspaoletti@gmail.com>
2025-05-22 08:59:49 -0700

    (neovim): Added nvim-tree file tree

--- modules/desktop/cli/nvim/lua/user/init.lua
+++ modules/desktop/cli/nvim/lua/user/init.lua
@@ -1,1 +1,9 @@
+-- disable netrw for file tree plugin
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
+
+-- plugins
 require("lazy").setup("user.plugins")
+
+-- ensure nvim-tree is properly loaded
+require("nvim-tree").setup({})
--- modules/desktop/cli/nvim/lua/user/plugins/init.lua
+++ modules/desktop/cli/nvim/lua/user/plugins/init.lua
@@ -1,5 +1,20 @@
 return {
 	{
+        -- file navigation/editing
+        {
+            "nvim-tree/nvim-tree.lua",
+            lazy = false,
+            dependencies = {
+                "nvim-tree/nvim-web-devicons",
+            },
+            keys = {
+                {
+                    "<leader>t",
+                    "<cmd>NvimTreeToggle<cr>",
+                    desc = "Toggle File Tree",
+                },
+            },
+        },
 		-- movement
 		{
 			"easymotion/vim-easymotion",
