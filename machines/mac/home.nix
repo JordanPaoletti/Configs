f0ce2ca1c613640ae765fd0cc1a3f2ca692df00b
Jordan Paoletti <paoletjo@amazon.com>
2025-10-09 13:58:59 -0700

    (nix): add nixfmt and luarocks to mac

--- machines/mac/home.nix
+++ machines/mac/home.nix
@@ -1,17 +1,22 @@
 { config, pkgs, ... }:
 
 {
-    imports = [
+  imports = [
     ../../modules/desktop/cli
-    ];
+  ];
 
   # Home Manager needs a bit of information about you and the paths it should
   # manage.
   home.username = "paoletjo";
   home.homeDirectory = "/Users/paoletjo";
 
+  home.packages = with pkgs; [
+    nixfmt-rfc-style
+    luajitPackages.luarocks
+  ];
+
   # https://nixos.wiki/wiki/FAQ/When_do_I_update_stateVersion
-  home.stateVersion = "25.05"; 
+  home.stateVersion = "25.05";
 
   # Let Home Manager install and manage itself.
   programs.home-manager.enable = true;
