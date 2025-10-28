84a1e2bbbc9379f7e14b87ef295ec2e79bb3efdf
Jordan Paoletti <paoletjo@amazon.com>
2025-10-28 12:40:59 -0700

    (mac): add zsh init for nvm

--- machines/mac/home.nix
+++ machines/mac/home.nix
@@ -26,6 +26,11 @@
     initContent = ''
       source ~/.brazil_completion/zsh_completion
       eval $(brew shellenv)
+
+      # setup nvm
+      export NVM_DIR="$HOME/.nvm"
+      [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+      [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
     '';
 
   };
