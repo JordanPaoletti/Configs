306663c15781fddaaf3db9c47209f9733e236214
Jordan Paoletti <paoletjo@amazon.com>
2026-06-29 10:58:31 -0700

    (nvim): enable spell checking on md, text, gitcommit

--- modules/desktop/cli/nvim/lua/user/init.lua
+++ modules/desktop/cli/nvim/lua/user/init.lua
@@ -35,3 +35,12 @@ vim.g.rainbow_delimiters = {
 		[""] = "rainbow-delimiters",
 	},
 }
+
+-- Spell check for prose filetypes
+vim.api.nvim_create_autocmd("FileType", {
+	pattern = { "markdown", "text", "gitcommit" },
+	callback = function()
+		vim.opt_local.spell = true
+		vim.opt_local.spelllang = "en_us"
+	end,
+})
